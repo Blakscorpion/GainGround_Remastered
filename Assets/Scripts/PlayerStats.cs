2c6c5c70573bd8b0269a2a5c448f8368b18a004b
Florian <florian.estour@gmail.com>
2023-03-30 00:42:22 +0200

    Baby is now registered for next level when exiting

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -2,8 +2,30 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    public string heroName;
+    public HeroesManager.Hero heroName;
     public int FragNumber = 0;
     public string heroDescription;
-    public bool isUnlocked;    
+    public bool hasABaby=false;   
+    public HeroesManager.Hero babyFollowing; 
+
+    private void Awake() {
+        GameManager.OnGameStateChanged += AddSuccessBaby;
+    }
+
+    private void OnDestroy() {
+        GameManager.OnGameStateChanged -= AddSuccessBaby;
+    }
+
+    private void AddSuccessBaby(GameState state) {
+        if(state == GameState.ExitSuccess && hasABaby==true)
+        {
+            HeroesManager.Instance.PassedHeros.Add(babyFollowing);
+            Debug.Log("Baby " + babyFollowing.ToString() +" has passed the exit");
+        }
+    }
+
+    public void setBabyFollowingName(HeroesManager.Hero baby)
+    {
+        babyFollowing = baby;
+    }
 }

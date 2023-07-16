4da62fc5f0904d0be12bb01cea16d1dae84f9923
Florian <florian.estour@gmail.com>
2023-07-16 23:20:19 +0200

    Dialogue system v1.0 Dialogue UI V1.0 Not yet synchronized together

---
+++ Assets/Scripts/DialogueScriptableObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue")]
+public class DialogueScriptableObject : ScriptableObject
+{
+    public HeroesManager.Hero[] requiredHeroesForDialogue;
+    public Dialogues[] dialogues;
+}
+
+[System.Serializable]
+public struct Dialogues
+{
+    public HeroesManager.Hero HeroName;
+    public Image HeroPortrait;
+    public string Dialogue;
+}
